f253a6a789d0ad5fe14c8056fd02876c074035b6
bihu8398 <bihu8398@eso1206.eso.local>
2014-07-14 15:21:18 +0200

    create project

---
+++ BasicUtils/src/cn/wagentim/basicutils/BasicUtils.java
@@ -0,0 +1,9 @@
+package cn.wagentim.basicutils;
+
+public class BasicUtils
+{
+	public static final boolean isNullOrEmpty(final String input)
+	{
+		return ( null == input ) || input.isEmpty();
+	}
+}
---
+++ BasicUtils/src/cn/wagentim/basicutils/StringConstants.java
@@ -0,0 +1,6 @@
+package cn.wagentim.basicutils;
+
+public interface StringConstants
+{
+	public static final String EMPTY_STRING = "";
+}
