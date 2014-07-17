2a657d3eb4ae4e9cfccd26de1c5a87ee041bae11
bihu8398 <bihu8398@eso1206.eso.local>
2014-07-17 13:14:06 +0200

    add text replacement function

--- BasicUtils/src/cn/wagentim/basicutils/StringConstants.java
+++ BasicUtils/src/cn/wagentim/basicutils/StringConstants.java
@@ -3,4 +3,6 @@ package cn.wagentim.basicutils;
 public interface StringConstants
 {
 	public static final String EMPTY_STRING = "";
+	public static final String SYMBOL_PERCENT = "%";
+	public static final String SPACE = " ";
 }
